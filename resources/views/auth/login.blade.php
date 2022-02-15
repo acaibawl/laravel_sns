a097d2854cefe687420c2ceeb53ed56a1cc8342a
acaiibawl <hagiya0719@gmail.com>
2022-02-15 15:52:54 +0900

    パスワード再設定要求画面の作成

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,7 +12,7 @@
             <h2 class="h3 card-title text-center mt-2">ログイン</h2>
 
             @include('error_card_list')
-            
+
             <div class="card-text">
               <form method="POST" action="{{ route('login') }}">
                 @csrf
@@ -26,10 +26,14 @@
                   <label for="password">パスワード</label>
                   <input class="form-control" type="password" id="password" name="password" required>
                 </div>
- 
-                {{--ここから--}}
+
                 <input type="hidden" name="remember" id="remember" value="on">
-                {{--ここまで--}}
+
+                {{-- ここから追加 --}}
+                <div class="text-left">
+                  <a href="{{ route('password.request') }}" class="card-text">パスワードを忘れた方</a>
+                </div>
+                {{-- ここまで追加 --}}
 
                 <button class="btn btn-block blue-gradient mt-2 mb-2" type="submit">ログイン</button>
 

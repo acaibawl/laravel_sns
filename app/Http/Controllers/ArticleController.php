0634328acfe2017ebd54188f9005d266283a9f66
acaiibawl <hagiya0719@gmail.com>
2022-02-16 16:24:16 +0900

    記事更新画面でタグを初期表示、更新できる

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -44,12 +44,24 @@ class ArticleController extends Controller
 
     public function edit(Article $article)
     {
-        return view('articles.edit', ['article' => $article]);
+        $tagNames = $article->tags->map(function ($tag) {
+            return ['text' => $tag->name];
+        });
+        
+        return view('articles.edit', ['article' => $article, 'tagNames' => $tagNames]);
     }
 
     public function update(ArticleRequest $request, Article $article)
     {
         $article->fill($request->all())->save();
+
+        // 一旦関連するタグをすべて削除してから、改めてリクエストの内容で作成する
+        $article->tags()->detach();
+        $request->tags->each(function ($tagName) use ($article) {
+            $tag = Tag::firstOrCreate(['name' => $tagName]);
+            $article->tags()->attach($tag);
+        });
+        
         return redirect()->route('articles.index');
     }
 
